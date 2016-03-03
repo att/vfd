5f42e0c3383cb4bbe8640e2c46198acbf1c122a8
Scott Daniels <daniels@research.att.com>
2016-03-03 09:19:16 -0500

    Remove jwrapper.h -- at this point it's build by mk.

--- src/lib/jwrapper.h
+++
@@ -1,7 +0,0 @@
-extern void jw_nuke( void* st );
-extern void* jw_new( char* json );
-extern char* jw_string( void* st, char* name );
-extern float jw_value( void* st, char* name );
-extern char* jw_string_ele( void* st, char* name, int idx );
-extern float jw_value_ele( void* st, char* name, int idx );
-extern int jw_array_len( void* st, char* name );
