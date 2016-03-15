e5d10ae0f7ab1866c75cef95bdd52f20b3c09372
Scott Daniels <daniels@research.att.com>
2016-03-15 11:33:03 -0400

    Make callback parameter null in sriov.c.

--- src/vfd/sriov.c
+++ src/vfd/sriov.c
@@ -436,7 +436,7 @@ port_init(uint8_t port, __attribute__((__unused__)) struct rte_mempool *mbuf_poo
 
   rte_eth_dev_callback_register(port, RTE_ETH_EVENT_INTR_LSC, lsi_event_callback, NULL);
   
-  rte_eth_dev_callback_register(port, RTE_ETH_EVENT_VF_MBOX, vf_msb_event_callback, (void *) &param);   
+  rte_eth_dev_callback_register(port, RTE_ETH_EVENT_VF_MBOX, vf_msb_event_callback, NULL);
 
 
 	// Allocate and set up 1 RX queue per Ethernet port. 
