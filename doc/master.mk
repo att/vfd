16a63c2a0a36f2d0ec5330d0df335e637c2dc4bd
Scott Daniels <daniels@research.att.com>
2017-02-06 14:19:48 -0500

    Add missing master.mmk to doc directory.

---
+++ doc/master.mk
@@ -0,0 +1,24 @@
+# master makefile for inclusion
+
+%.eps: %.fig
+    fig2dev -L eps ${prereq} ${target}
+
+%.png: %.fig
+    fig2dev -L png ${prereq} ${target}
+
+
+%_slides :  %_slides.xfm
+	pfm -g 8ix11i ${prereq%% *} $target.ps
+
+%.ps :  %.xfm
+	pfm ${prereq%% *} $target 
+
+%.html :  %.xfm
+	hfm ${prereq%% *} $target 
+
+%.pdf : %.ps
+	gs -I/usr/local/share/ghostscript/fonts -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -sOutputFile=$target ${prereq% *}
+
+%.txt : %.xfm
+	tfm ${prereq%% *} $target 
+
