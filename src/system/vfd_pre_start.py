38ecd7401b4a42e356ccc8a440c4c9a51c7daab0
Dhanunjaya Naidu Ravada <dr3662@att.com>
2017-02-10 03:27:30 -0500

    this script will handle pf, vf bind to vfio-pci

--- src/system/vfd_pre_start.py
+++ src/system/vfd_pre_start.py
@@ -2,16 +2,15 @@
 # vi: sw=4 ts=4:
 
 """
-    Mnemonic:       vfd_pre_start.py
-    Abstract:       This script calls the 'dpdk_nic_bind' script to bind PF's and VF's to vfio-pci
-    Date:           April 2016
-    Author:         Dhanunjaya Naidu Ravada (dr3662@att.com)
-    Mod:            2016 7 Apr - Created script
-                    2016 8 Apr - fix to index out of bound error
-                    2016 22 Apr - remove unloading ixgbevf driver
-                    2016 30 May - wait for vf's to create
-                    2016 8 Dec - fix to detrmine which iommu group pciid belong to and get the other pciid's in that iommu group.
-                    2017 3 Feb - Add support to bind pf's to igb_uio.ko (dpdk)
+    Mnemonic:   vfd_pre_start.py
+    Abstract:   This script calls the 'dpdk_nic_bind' script to bind PF's and VF's to vfio-pci
+    Date:       April 2016
+    Author:     Dhanunjaya Naidu Ravada (dr3662@att.com)
+    Mod:        2016 7 Apr - Created script
+                2016 8 Apr - fix to index out of bound error
+                2016 22 Apr - remove unloading ixgbevf driver
+                2016 30 May - wait for vf's to create
+                2017 10 Feb - update the code with parallel execution of VF's bind
 """
 
 import subprocess
@@ -27,15 +26,6 @@ VFD_CONFIG = '/etc/vfd/vfd.cfg'
 SYS_DIR = "/sys/devices"
 LOG_DIR = '/var/log/vfd'
 
-# global varibale which will hold vfd.cfg as json data
-data = {}
-
-# max_vfs path
-file_path = ""
-
-# sometimes after binding pf to igb_uio still vfs exist
-is_igb_uio = False
-
 # logging
 log = logging.getLogger('vfd_pre_start')
 
@@ -45,30 +35,6 @@ def setup_logging(logfile):
     handler.setFormatter(log_formatter)
     log.setLevel(logging.INFO)
     log.addHandler(handler)
-
-def is_uio_loaded():
-    try:
-        subprocess.check_call('lsmod | grep uio >/dev/null', shell=True)
-        return True
-    except subprocess.CalledProcessError:
-        return False
-
-def load_uio():
-    try:
-        subprocess.check_call('modprobe uio', shell=True)
-        return True
-    except subprocess.CalledProcessError:
-        return False
-
-def is_igb_uio_loaded():
-    try:
-        subprocess.check_call('lsmod | grep igb_uio >/dev/null', shell=True)
-        return True
-    except subprocess.CalledProcessError:
-        return False
-
-def load_igb_uio():
-    pass
 
 def is_vfio_pci_loaded():
     try:
@@ -86,21 +52,21 @@ def load_vfio_pci_driver():
         return False
 
 # get pciids from vfd.cfg
-def get_config():
-    global data
+def get_pciids():
     with open(VFD_CONFIG) as data_file:
         try:
             data = json.load(data_file)
         except ValueError:
             log.error("%s is not a valid json", VFD_CONFIG)
             sys.exit(1)
+    return data['pciids']
 
-# unbind pf and vf
-def unbind_pfs_vfs(dev_id):
-    unbind_cmd = 'dpdk_nic_bind --force -u %s' % dev_id
-    log.info(unbind_cmd)
+# unbind pciid
+def unbind_pfs(dev_id):
+    unbind_cmd = "dpdk_nic_bind --force -u %s" % dev_id
     try:
         msg = subprocess.check_output(unbind_cmd, shell=True)
+        log.info(unbind_cmd)
         if "Routing" in msg:
             log.error(msg)
             return False
@@ -114,68 +80,18 @@ def get_vfids(dev_id):
     log.info("[%s]: %s", dev_id, vfids)
     return filter(None, vfids)
 
-def get_vfs_path(dev_id):
-    global file_path
-    cmd = "find /sys -name *vfs | grep %s | grep max_vfs" % dev_id
-    log.info(cmd)
-    try:
-        file_path = subprocess.check_output(cmd, shell=True)
-        return True
-    except subprocess.CalledProcessError:
-        return False
-
-def create_vfs(dev_id, vfs):
-    get_vfs_path(dev_id)
-    global file_path
-    if file_path:
-        cmd = "echo '%s' > %s" % (str(vfs), file_path)
-        log.info(cmd)
-        try:
-            if is_igb_uio and len(get_vfids(dev_id)):
-                return True
-            else:
-                subprocess.check_call(cmd, shell=True)
-                return True
-        except subprocess.CalledProcessError:
-            return False
-    else:
-        return False
-
-# bind pf's to igb_uio
-def bind_pf(dev_id):
-    bind_cmd = 'dpdk_nic_bind --force -b igb_uio %s' % dev_id
-    log.info(bind_cmd)
+# bind pf's and vf's to vfio-pci
+def bind_pf_vfs(dev_id):
+    bind_cmd = "dpdk_nic_bind --force -b vfio-pci %s" % dev_id
     try:
         subprocess.check_call(bind_cmd, shell=True)
+        log.info(bind_cmd)
         return True
     except subprocess.CalledProcessError:
         return False
 
-# bind vf's to vfio-pci
-def bind_vf(dev_id):
-    bind_cmd = 'dpdk_nic_bind --force -b vfio-pci %s' % dev_id
-    log.info(bind_cmd)
-    try:
-        subprocess.check_call(bind_cmd, shell=True)
-        return True
-    except subprocess.CalledProcessError:
-        return False
-
-# check whether igb_uio driver is attached to pf
-def driver_attach_pf(dev_id):
-    index = 0
-    cmd = 'lspci -k -s %s' % dev_id
-    try:
-        driver_name = subprocess.check_output(cmd, shell=True).splitlines()[2].split(':')[1].lstrip()
-        if driver_name == 'igb_uio':
-            return [index, True]
-        return [index, False]
-    except IndexError:
-        index = 1
-        return [index, False]
-
-# check whether vfio-pci driver is attached to vf
-def driver_attach_vf(dev_id):
+# check whether vfio-pci driver is attached to pf or vf
+def driver_attach(dev_id):
     index = 0
     cmd = 'lspci -k -s %s' % dev_id
     try:
@@ -187,7 +103,7 @@ def driver_attach_vf(dev_id):
         index = 1
         return [index, False]
 
-# get the pci cards in the group which must be attached to the igb_uio driver
+# get the pci cards in the group which must be attached to the vfio-pci driver
 def get_pciids_group(dev_id):
     group_pciids = []
     group_num = None
@@ -216,55 +132,34 @@ def check_vendor(pciids):
             sys.exit(1)
     return not_ixgbe_vendor
 
-def get_configured_vfs(pciids):
-    vfd_count = 0
-    for pciid in pciids:
-        vfd_count = vfd_count + len(get_vfids(pciid))
-    return vfd_count
-
 if __name__ == '__main__':
     setup_logging('vfd_upstart.log')
-    
-    get_config()
-    
+
     pciids = []
-    for value in data['pciids']:
+    for value in get_pciids():
         if 'id' in value:
             pciids.append(value['id'])
         else:
             pciids.append(value)
-            
+
     group_pciids = []
     for pciid in pciids:
         group_pciids = group_pciids + get_pciids_group(pciid)
-        
+
     pciids = list(set(pciids) | set(group_pciids))
     log.info("pciids: %s", pciids)
-    
+
+#    for pciid in pciids:
+#        vfs_count = len(get_vfids(pciid))
+#        if vfs_count < 32:
+#            log.error("It seems 32 VF's are not Created for this pf [%s] check 'dpdk_nic_bind --st'", pciid)
+#            sys.exit(1)
+
     not_ixgbe_vendor = check_vendor(pciids)
     if len(not_ixgbe_vendor) > 0:
-        log.error("VFD won't handle for this vendors: %s", not_ixgbe_vendor)
+        log.error("VFD wont handle for this vendors: %s", not_ixgbe_vendor)
         sys.exit(1)
-    
-    if not is_uio_loaded():
-        if load_uio():
-            log.info("Successfully loaded uio driver")
-        else:
-            log.error("unable to load uio driver")
-            sys.exit(1)
-    else:
-        log.info("Already loaded uio driver")
-    
-    if not is_igb_uio_loaded():
-        log.error("please load dpdk igb_uio driver")
-        sys.exit(1)
-    
-    if data["enable_qos"]:
-        for pciid in pciids:
-            if len(get_vfids(pciid)) < 0 and len(get_vfids(pciid)) > 31:
-                log.error("qos supports (0-31) vfs per pf, pf %s: vf %s", pciid, str(len(get_vfids(pciid))))
-                sys.exit(1)                
-    
+
     if not is_vfio_pci_loaded():
         if load_vfio_pci_driver():
             log.info("Successfully loaded vfio-pci driver")
@@ -273,52 +168,39 @@ if __name__ == '__main__':
             sys.exit(1)
     else:
         log.info("Already loaded vfio-pci driver")
-        
+
     for pciid in pciids:
-        vfs_count = len(get_vfids(pciid))
-        if vfs_count > 0:
-            status_list = driver_attach_pf(pciid)
-            if not status_list[1]:
-                if status_list[0] == 0:
-                    if not unbind_pfs_vfs(pciid):
-                        log.error("unable to unbind %s PF", pciid)
+        status_list = driver_attach(pciid)
+        if not status_list[1]:
+            if status_list[0] == 0:
+                if not unbind_pfs(pciid):
+                    log.error("unable to unbind %s PF", pciid)
+                    sys.exit(1)
+                else:
+                    log.info("Successfully unbind %s", pciid)
+                    if not bind_pf_vfs(pciid):
+                        log.error("unable to bind %s with vfio-pci", pciid)
                         sys.exit(1)
                     else:
-                        log.info("Successfully unbind %s", pciid)
-                        if not bind_pf(pciid):
-                            log.error("unable to bind %s with igb_uio", pciid)
-                            sys.exit(1)
-                        else:
-                            log.info("Successfully bind %s", pciid)
-                            global is_igb_uio
-                            is_igb_uio = True
-                            if not create_vfs(pciid, vfs_count):
-                                log.error("not able to create vfs for pf %s", pciid)
-                                sys.exit(1)
-                            else:
-                                log.info("Successfully created vfs for pf: %s", pciid)
-            else:
-                log.info("Already %s bind to igb_uio driver", pciid)
+                        log.info("Successfully bind %s", pciid)
         else:
-            log.error("vfs are not created for pf : %s, vfs: %s", pciid, str(vfs_count))
-            sys.exit(1)
-            
+            log.info("Already %s bind to vfio-pci driver", pciid)
+
+    jobs = []
     for pciid in pciids:
-        jobs = []
         for vfid in get_vfids(pciid):
-            return_list = driver_attach_vf(vfid)
+            return_list = driver_attach(vfid)
             if not return_list[1]:
                 if return_list[0] == 0:
-                    t = multiprocessing.Process(target=unbind_pfs_vfs, args=(vfid,))
+                    t = multiprocessing.Process(target=unbind_pfs, args=(vfid,))
                     jobs.append(t)
                     t.start()
-        for j in jobs:
-            j.join()
-            
+    for job in jobs:
+        job.join()
+
     for pciid in pciids:
         for vfid in get_vfids(pciid):
-            return_list = driver_attach_vf(vfid)
+            return_list = driver_attach(vfid)
             if not return_list[1]:
                 if return_list[0] == 0 or return_list[0] == 1:
-                    t = multiprocessing.Process(target=bind_vf, args=(vfid,))
-                    t.start()
+                    bind_pf_vfs(vfid)
